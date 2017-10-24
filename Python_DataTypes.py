abfe6d8d69c3d04d529c0ad971df08a88cb36c22
satishavirneni <satish.avirneni88@gmail.com>
2017-10-24 12:43:29 -0400

    first commit

---
+++ Python_DataTypes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+
+accountID = 1233
+balance = 33.45
+com = complex(3, 4)
+
+print(float(accountID),type(accountID),id(accountID))
+
+print(int(balance),type(balance),id(balance))
+
+print (com.real,type(com),id(com))
+
+print (com.imag,type(com),id(com))
+
+print (com.conjugate(),type(com),id(com))
